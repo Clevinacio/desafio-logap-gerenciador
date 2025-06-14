3f70fd5438cdc099ef5b9b510947c26b84611ea2
Cleverton Inácio <clev.inacio@gmail.com>
2025-06-13 21:08:26 -0300

    permite definir cors dinamicamente

--- backend/src/main/java/com/logap/teste/gerenciadorbackend/configuration/CorsConfig.java
+++ backend/src/main/java/com/logap/teste/gerenciadorbackend/configuration/CorsConfig.java
@@ -1,15 +1,22 @@
 package com.logap.teste.gerenciadorbackend.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig implements  WebMvcConfigurer {
+
+    @Value("${cors.allowed.origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+        registry.addMapping(Arrays.toString(allowedOrigins))
+                .allowedOrigins("")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
